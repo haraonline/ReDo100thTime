3a2b9b7b8030f408eead4497605d49e9155810d0
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-10 16:09:47 +0200

    add a constructor

--- 01_Basics/Program.cs
+++ 01_Basics/Program.cs
@@ -6,52 +6,12 @@ namespace _01_Basics
     {
         static void Main(string[] args)
         {
-            Warrior goodGuy = new Warrior();
-            goodGuy.Name = "Bahu";
-            goodGuy.Height = 180;
-            goodGuy.Weight= 90;
-            goodGuy.Introduce();            
-            
+            Warrior goodGuy = new Warrior(name: "Bahu", height: 180, weight: 90);
+            Warrior badGuy = new Warrior(name: "Bhalla", height: 200, weight: 100);
 
-            Warrior badGuy = new Warrior();
-            badGuy.Name = "Bhalla";
-            badGuy.Height= 200;
-            badGuy.Weight = 100;
+            goodGuy.Introduce();
             badGuy.Introduce();
-
             goodGuy.ShakesHand(goodGuy, badGuy);
-        }
-    }
-
-    public class Warrior
-    {
-        //These are properties and are generally for external use (values are assigned outside the class)
-
-        public string Name { get; set; }
-        public int Height { get; set; }
-        public int Weight { get; set; }
-
-        //These are fields and are for internal use only
-        private readonly string country = "Magadha";
-        private readonly int population = 3_050;
-
-        //Methods
-        public void Introduce()
-        {
-            Console.WriteLine($"Inroducting {Name}");
-            Console.WriteLine($"Height: {Height}");
-            Console.WriteLine($"Weight: {Weight}");
-            Console.WriteLine($"Country: {country}");
-            Console.WriteLine($"Population: {population}");
-            Console.WriteLine();
-            Console.WriteLine("--------------------");
-            Console.WriteLine();
-        }
-
-        public void ShakesHand(Warrior w1, Warrior w2)
-        {
-            Console.WriteLine($"{w1.Name} says hello to {w2.Name}");
-            Console.WriteLine();
         }
     }
 }
---
+++ 01_Basics/Warrior.cs
@@ -0,0 +1,40 @@
+﻿namespace _01_Basics
+{
+    public class Warrior
+    {
+        //These are properties and are generally for external use (values are assigned from outside the class)
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        //These are fields and are for internal use only
+        private readonly string country = "Magadha";
+        private readonly int population = 3_050;
+
+        //Methods
+        public void Introduce()
+        {
+            Console.WriteLine($"Inroducting {Name}");
+            Console.WriteLine($"Height: {Height}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine($"Country: {country}");
+            Console.WriteLine($"Population: {population}");
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+            Console.WriteLine();
+        }
+
+        public void ShakesHand(Warrior w1, Warrior w2)
+        {
+            Console.WriteLine($"{w1.Name} says hello to {w2.Name}");
+            Console.WriteLine();
+        }
+
+        public Warrior(string name, int height, int weight)        
+        {
+            Name = name;
+            Height = height;
+            Weight = weight;
+        }
+    }
+}
