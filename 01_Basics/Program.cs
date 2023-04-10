7818d8e1bf8af239f7d98c8cad3d1314d4a97336
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-10 12:59:36 +0200

    add fields, properties, methods

--- 01_Basics/Program.cs
+++ 01_Basics/Program.cs
@@ -1,32 +1,49 @@
-﻿namespace _01_Basics
+﻿using System.Globalization;
+
+namespace _01_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Warrior goodGuy = new Warrior();
+            goodGuy.Name = "Bahu";
             goodGuy.Height = 180;
             goodGuy.Weight= 90;
+            goodGuy.Introduce();
             
 
             Warrior badGuy = new Warrior();
+            badGuy.Name = "Bhalla";
             badGuy.Height= 200;
-            badGuy.Weight = 100;         
-
+            badGuy.Weight = 100;
+            badGuy.Introduce();
         }
     }
 
     public class Warrior
     {
-        //these are properties and are generally for external use (values are assigned outside the class)
+        //These are properties and are generally for external use (values are assigned outside the class)
+
+        public string Name { get; set; }
         public int Height { get; set; }
         public int Weight { get; set; }
 
-        //these are fields and are for internal use only
+        //These are fields and are for internal use only
         public string country = "Magadha";
         public int population = 3_050;
 
+        //Methods
+        public void Introduce()
+        {
+            Console.WriteLine($"Inroducting {Name}");
+            Console.WriteLine($"Height: {Height}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine($"Rajyam: {country}");
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+            Console.WriteLine();
+        }       
 
-        
     }
 }
