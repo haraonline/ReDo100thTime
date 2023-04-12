65f4cf575639f472b6c0929c2f5cd37eaf40d12a
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-12 15:23:24 +0200

    access modifiers

---
+++ 02.Accessors/Bank.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Accessors
+{
+    public class Bank
+    {
+        public string Name { get; set; }
+        public string Address { get; private set; }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 18 && value <= 100)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Argument: Age, Age must be between 18 and 100");
+                };
+            }
+        }
+
+
+        public Bank(string name, string address, int age)
+        {
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+
+        public void ChangeAddress(string newAddress)
+        {
+            Address = newAddress;
+        }
+
+        public override string ToString()
+        {
+            return $"Bank Name: {Name}, Address: {Address}, Age: {Age}";
+        }
+    }    
+}
---
+++ 02.Accessors/Program.cs
@@ -0,0 +1,13 @@
+﻿using _02.Accessors;
+
+namespace _02_Accessors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bank = new Bank("Bank of America", "123 Main St.", 17);            
+            Console.WriteLine(bank);
+        }
+    }
+}
