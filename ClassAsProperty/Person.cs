0d7ee8b45d7d67743420961c8dc88bb355800e69
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-12 15:52:48 +0200

    class as a property

---
+++ ClassAsProperty/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAsProperty
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Address HomeAddress { get; set; }
+
+        public Person(string firstName, string lastName, DateTime birthDate, Address homeAddress)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            HomeAddress = homeAddress;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} was born on {BirthDate.ToShortDateString(),10} and lives at {HomeAddress}";
+        }
+    }    
+}
---
+++ ClassAsProperty/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassAsProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address addr1 = new Address("123 Main St", "Hamburg");
+            Person person1 = new Person("John", "Doe", new DateTime(1980, 1, 1), addr1);
+            Console.WriteLine(person1);
+        }
+    }
+}
