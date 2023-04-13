74c6f18309e6d488bbfef1c89c23fa1d81ce7219
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-13 14:23:35 +0200

    chaining constructors

---
+++ 04_ConstructorChaining/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ConstructorChaining
+{
+    internal class Person
+    {        
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        //Chain constructors
+        public Person() : this("No Name", 0, "No Address") { }
+        public Person(string name) : this(name, 0, "No Address") { }
+        public Person(string name, int age) : this(name, age, "No Address") { }
+        public Person(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+
+
+    }
+}
---
+++ 04_ConstructorChaining/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _04_ConstructorChaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+            Person p2 = new Person("John");
+            Person p3 = new Person("John", 25);
+            Person p4 = new Person("John", 25, "123 Main St");
+
+            p1.Address = "123 Main St";
+            p1.Age = 25;
+            p1.Name = "John";
+
+            p2.Address = "123 Main St";
+            p2.Age = 25;
+
+            p3.Address = "123 Main St";
+
+            System.Console.WriteLine("p1: {0}, {1}, {2}", p1.Name, p1.Age, p1.Address);
+            System.Console.WriteLine("p2: {0}, {1}, {2}", p2.Name, p2.Age, p2.Address);
+            System.Console.WriteLine("p3: {0}, {1}, {2}", p3.Name, p3.Age, p3.Address);
+            System.Console.WriteLine("p4: {0}, {1}, {2}", p4.Name, p4.Age, p4.Address);
+            
+            
+
+
+
+            System.Console.ReadLine();
+        }
+    }
+}
