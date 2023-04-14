604970252cc9defae92a9307f5be3036fe64fd07
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-14 14:10:19 +0200

    instance counter

---
+++ 05_StaticClassesAndMembers/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace _05_StaticClassesAndMembers
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public static int Count { get; set; }
+
+        public Person(string name, string lastName, int age)
+        {
+            Name = name;
+            LastName = lastName;
+            Age = age;
+            Count++; //this key word doesnt apply here or to the static members. It points to a specific instance of the class.
+                     //Static members are shared by all instances of the class.
+        }
+
+        public static void PrintCount()
+        {
+            Console.WriteLine($"Count: {Count}");
+        }
+    }
+}
---
+++ 05_StaticClassesAndMembers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05_StaticClassesAndMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("John", "Doe", 30);
+            Person p2 = new Person("Jane", "Doe", 25);
+            Person p3 = new Person("Jack", "Doe", 20);
+            Person p4 = new Person("Jill", "Doe", 15);
+            Person p5 = new Person("Jenny", "Doe", 10);
+            Person p6 = new Person("Jenny", "Doe", 10);
+            Person p7 = new Person("Jenny", "Doe", 10);
+            Person p8 = new Person("Jenny", "Doe", 10);
+            Person.PrintCount();
+            Person p9 = new Person("Jenny", "Doe", 10);
+            Person p10 = new Person("Jenny", "Doe", 10);
+
+            Person.PrintCount();
+        }
+    }
+}
