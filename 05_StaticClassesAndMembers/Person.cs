6ac7a379ad8e0e4f7b9899367098c8d8cd437240
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-14 14:33:36 +0200

    static, readonly, private

--- 05_StaticClassesAndMembers/Person.cs
+++ 05_StaticClassesAndMembers/Person.cs
@@ -2,6 +2,15 @@
 {
     internal class Person
     {
+        private readonly int id;
+
+        public int Id
+        {
+            get { return id; }
+            //private set { id = value; } //set the value internally within the class constructor not while instantiation outside the class
+                                        //if this field is private readonly, you cannt set this in the constructor. therefore remove if its readonly
+        }
+    
         public string Name { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
@@ -9,16 +18,23 @@
 
         public Person(string name, string lastName, int age)
         {
+            Count++; //this key word doesnt apply here or to the static members. It points to a specific instance of the class.
+                     //Static members are shared by all instances of the class.
+                     //Id = Count; //if the field is readonly, we cannot set the property but we can only set the private field as show below (lower case)
+            id = Count;
             Name = name;
             LastName = lastName;
             Age = age;
-            Count++; //this key word doesnt apply here or to the static members. It points to a specific instance of the class.
-                     //Static members are shared by all instances of the class.
         }
-
+        
         public static void PrintCount()
         {
             Console.WriteLine($"Count: {Count}");
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, LastName: {LastName}, Age: {Age}";
         }
     }
 }
--- 05_StaticClassesAndMembers/Program.cs
+++ 05_StaticClassesAndMembers/Program.cs
@@ -5,16 +5,18 @@
         static void Main(string[] args)
         {
             Person p1 = new Person("John", "Doe", 30);
-            Person p2 = new Person("Jane", "Doe", 25);
-            Person p3 = new Person("Jack", "Doe", 20);
-            Person p4 = new Person("Jill", "Doe", 15);
-            Person p5 = new Person("Jenny", "Doe", 10);
-            Person p6 = new Person("Jenny", "Doe", 10);
-            Person p7 = new Person("Jenny", "Doe", 10);
-            Person p8 = new Person("Jenny", "Doe", 10);
+            Console.WriteLine(p1);
+            Person p2 = new Person("Anita", "Kimmel", 25);
+            Person p3 = new Person("Jack", "Ryan", 20);
+            Person p4 = new Person("Jill", "Mendoz", 15);
+            Person p5 = new Person("Jenny", "Swarznegger", 10);
+            Person p6 = new Person("Mary", "Littlelamb", 10);
+            Console.WriteLine(p6);
+            Person p7 = new Person("Monika", "Schmidt", 10);
+            Person p8 = new Person("Antonio", "Fukua", 10);
             Person.PrintCount();
-            Person p9 = new Person("Jenny", "Doe", 10);
-            Person p10 = new Person("Jenny", "Doe", 10);
+            Person p9 = new Person("Hara guchi", "Mai", 10);
+            Person p10 = new Person("Tina", "Munim", 10);
 
             Person.PrintCount();
         }
