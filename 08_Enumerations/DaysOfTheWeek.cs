b7abd76ae3f5cde4bb7ea9590bc05e7b51db8df5
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-15 19:45:48 +0200

    enumerations and bitwise operations

---
+++ 08_Enumerations/DaysOfTheWeek.cs
@@ -0,0 +1,14 @@
+﻿namespace _08_Enumerations
+{
+    [Flags]
+    public enum DayOfTheWeek
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 4,
+        Thursday = 8,
+        Friday = 16,
+        Saturday = 32,
+        Sunday = 64
+    }    
+}
---
+++ 08_Enumerations/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _08_Enumerations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Doe";
+            person.WorksOn = DayOfTheWeek.Monday | DayOfTheWeek.Tuesday | DayOfTheWeek.Wednesday | DayOfTheWeek.Thursday | DayOfTheWeek.Friday;
+            Console.WriteLine(person);
+
+        }      
+    }
+
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DayOfTheWeek WorksOn { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " works on " + WorksOn;
+        }
+    }    
+}
