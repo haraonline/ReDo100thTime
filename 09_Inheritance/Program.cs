9da00fdf0f64ed78761653eda96e87097413df54
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-16 14:22:16 +0200

    is-a and has-a

--- 09_Inheritance/Program.cs
+++ 09_Inheritance/Program.cs
@@ -9,6 +9,17 @@
             Console.WriteLine(cat.Sound);
             Console.WriteLine(dog.Sound);
 
+            //is a or has a relationship (a - for class, has a - for properties)
+            //Cat is an Animal
+            //Dog is an Animal
+            //Animal has a Sound
+            //Cat has a Sound
+            //Dog has a Sound
+            //Cat is a Mammal
+            //Dog is a Mammal
+            //Mammal has a Sound
+            //Cat has a Sound
+
         }
     }
 }
