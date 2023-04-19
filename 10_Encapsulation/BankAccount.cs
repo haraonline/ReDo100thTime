db86bec57382258623690984a92329ce0c0a811c
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-19 13:51:37 +0200

    Encapsulation

---
+++ 10_Encapsulation/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿namespace _10_Encapsulation
+{
+    internal class BankAccount
+    {
+        private decimal _balance;
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            private set
+            {
+                if (value >= 0)         
+                    _balance = value;                
+            }
+        }
+        public string Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+            }
+                
+            return $"deposited {amount}, your new balance is {Balance}";
+        }
+
+        public string Withdraw(decimal amount)            
+        {
+            if (amount > 0 && Balance >= amount)
+            {
+                Balance -= amount;
+                return $"withdrawing {amount}, your new balance is {Balance}";
+            }
+            else
+            {
+                return $"Insufficient balance - Balance: {Balance}, Withdrawl requested: {amount}";
+            }
+        }
+    }
+}
---
+++ 10_Encapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _10_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            string deposit = account.Deposit(1000);
+            string w1 = account.Withdraw(25);            
+            string w2 = account.Withdraw(2000);
+
+
+            Console.WriteLine(deposit);
+            Console.WriteLine(w1);
+            Console.WriteLine("Balance: " + account.Balance);
+            Console.WriteLine(w2);
+            Console.WriteLine("Balance: " + account.Balance);
+        }
+    }
+}
